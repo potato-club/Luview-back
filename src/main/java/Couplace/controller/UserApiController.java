f625dbabb0a6bd7db9a17cf288ef0214d7c9c91c
jihun4452 <jihun6548@naver.com>
2024-08-26 14:13:28 +0900

    소셜로그인 구현

---
+++ src/main/java/Couplace/controller/UserApiController.java
@@ -0,0 +1,20 @@
+package Couplace.controller;
+
+import Couplace.repository.AddUserRequest;
+import Couplace.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+
+public class UserApiController {
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request) {
+        userService.save(request);
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/Couplace/service/UserService.java
@@ -0,0 +1,22 @@
+package Couplace.service;
+
+import Couplace.entity.User;
+import Couplace.repository.AddUserRequest;
+import Couplace.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
