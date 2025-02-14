8cd3e1f39723e433b09118b2f4fc367c974c0308
박지훈 <jihun6548@naver.com>
2025-02-14 16:20:56 +0900

    fix: MailController수정

--- src/main/java/solo/project/controller/MailController.java
+++ src/main/java/solo/project/controller/MailController.java
@@ -17,19 +17,19 @@ import solo.project.service.MailService;
 @RestController
 @RequestMapping("/mail")
 @RequiredArgsConstructor
-@Tag(name="Email Controller" ,description = "이메일 인증 API입니다.")
+@Tag(name="Email Controller" ,description = "이메일 인증 API")
 @Slf4j
 public class MailController {
     private final MailService mailService;
 
-    @Operation(summary = "Gmail 인증번호 발송")
+    @Operation(summary = "Email 인증번호 발송")
     @PostMapping("/send")
     public ResponseEntity<String> sendEmail(String ToEmail) {
         mailService.sendEmail(ToEmail);
         return ResponseEntity.ok("입력하신 이메일로 인증번호가 전달되었습니다.");
     }
 
-    @Operation(summary = "인증 코드 확인 API")
+    @Operation(summary = "인증 코드 확인")
     @PostMapping("/verify")
     public ResponseEntity<String> verifyEmail(String key, HttpServletResponse response) {
         mailService.verifyEmail(key, response);
